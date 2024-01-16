a584c9e1c9e03eb9bab1f30a79d306d7d600e40d
Carlos Cabi <carlangascamachobrigido@gmail.com>
2024-01-15 20:54:15 -0500

    finally. done in `index.js`

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const dbConnection = require('./database/dbConnect.js')
 const validateUrl = require('./schemas/shortUrl/url.js')
 const bodyParser = require('body-parser')
 const dns = require('dns');
+const urlParser = require('url');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(express.json())
 
 dbConnection()
 
@@ -43,73 +45,41 @@ app.get('/api/hello', function(req, res) {
 });
 
 const ERRORS_URLS_MESSAGES = {
-  ERR_INVALID_URL: 'invalid url',
+  ERR_INVALID_URL: 'Invalid URL',
   ERR_DNS_NOT_FOUND: 'dns not found',
-}
-
-const SUCCESS_URLS_MESSAGES = {
-  DNS_SUCCESSFULLY_FOUND: 'dns successfully found',
-}
-
-// To avoid ENOTFOUND error
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // When I post a url I...
 app.post("/api/shorturl", async (req, res) => { 
   const { url } = req.body
   const validate = validateUrl({ url })
-
-  if(!validate.success) return res.status(404).json({ error: ERRORS_URLS_MESSAGES.ERR_INVALID_URL })
   
-  const REPLACE_REGEX = /^https?:\/\//i
-  const urlWithoutProtocol = url.replace(REPLACE_REGEX, '')
-
-  const urlResolver = async () => { 
-      await sleep(1)
-      const dnsResolved = dns.resolve(urlWithoutProtocol, async (error, address, family) => {
-        if (error) {
-          return {
-            error: true,
-            message: ERRORS_URLS_MESSAGES.ERR_DNS_NOT_FOUND,
-            response: null,
-          }
-        } else { 
-          console.log(`Ip of the address: ${address} and ip:config: ${family}`)
-          return {
-              error: null,
-              message: SUCCESS_URLS_MESSAGES.DNS_SUCCESSFULLY_FOUND,
-              response: {
-                address,
-                family
-              }
-          }
-        }
-      
-      });
-
-      return dnsResolved
-    }
-
+  if(!validate.success) return res.json({ error: ERRORS_URLS_MESSAGES.ERR_INVALID_URL })
   
+  const urlWithoutProtocol = urlParser.parse(url).hostname
   
-  try {
-    const { callback } = await urlResolver()
-    const dnsValid = await callback()
-
-    if (dnsValid.error) return res.status(404).json({ error: ERRORS_URLS_MESSAGES.ERR_INVALID_URL })
-
-
-    const collectionLength = await UrlModel.countDocuments()
-
-    const newShortUrl = await UrlModel.create({ original_url: url, short_url: collectionLength + 1 })
+  console.log(`URL ORIGINAL: ${url} TO ${urlWithoutProtocol}`)
+  
+  if (urlWithoutProtocol === undefined) return res.json({ error: ERRORS_URLS_MESSAGES.ERR_INVALID_URL })
+  if (urlWithoutProtocol === null) return res.json({ error: ERRORS_URLS_MESSAGES.ERR_INVALID_URL })
+  
+  try { 
+    dns.lookup(urlWithoutProtocol, async (error, address, family) => { 
+      if (!address) {
+        res.json({ error: ERRORS_URLS_MESSAGES.ERR_INVALID_URL })
+      } else { 
+        console.log(`Ip of the address: ${address} and ip:config: ${family}`)
+        const collectionLength = await UrlModel.countDocuments()
     
-    const { original_url, short_url } = newShortUrl
-
-    return res.status(200).json({
-      original_url,
-      short_url
+        const newShortUrl = await UrlModel.create({ original_url: url, short_url: collectionLength + 1 })
+        
+        const { original_url, short_url } = newShortUrl
+    
+        return res.status(200).json({
+          original_url,
+          short_url
+        })
+      }
     })
   } catch (err) {
     console.log(err)
@@ -125,7 +95,9 @@ app.get("/api/shorturl/:url", async (req, res) => {
   } catch (err) {
       return res.json({ error: ERRORS_URLS_MESSAGES.ERR_INVALID_URL })
   }
- })
+})
+ 
+app.get("*", (req, res) => res.json({ message: "Visit another endpoint!!"}))
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
